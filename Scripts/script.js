59d7f4a18b15823539428d93f4e8523c774a60d5
César Fernández <56650023+xJekk@users.noreply.github.com>
2023-03-06 11:55:04 +0100

    Add files via upload

---
+++ Scripts/script.js
@@ -0,0 +1,191 @@
+
+/*Módulo Burger Menú*/
+
+const burgerBtn = document.getElementById("burger-btn");
+const menu = document.querySelector(".menu");
+
+burgerBtn.addEventListener("click", () => {
+    menu.classList.toggle("menu-desplegable");
+}); 
+
+//Módulo percentage bar
+const scrollBar = document.getElementById("scroll-bar");
+
+window.addEventListener("scroll", () => {
+  let percent = calcPercentage();  
+  scrollBar.style.width = `${percent}%`;
+});
+
+function calcPercentage() {
+    let scrollTop = window.scrollY;
+    let docHeight = document.body.offsetHeight;
+    let winHeight = window.innerHeight;
+    let scrollPercent = scrollTop / (docHeight - winHeight);
+    let percent = Math.round(scrollPercent * 100);
+    return percent;
+}
+
+//Back to top button
+
+const backTop = document.getElementById("return-top");
+
+backTop.addEventListener("click", () => {
+     setTimeout(() => {
+        document.documentElement.scrollTop = 0;
+    },200); 
+})
+
+//Formulario
+const form  = document.getElementById("form");
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const checkbox = document.getElementById("consent");
+
+
+nombre.addEventListener("blur", () => {  //Blur para que se ponga rojo al salir del campo y no compruebe nada más clicar
+    if (!(validateName())) {
+        nombre.style.borderColor = "red";
+    }
+});
+email.addEventListener("blur", () => {
+    if ((!validateMail())) {
+        email.style.borderColor = "red";
+    }
+});
+checkbox.addEventListener("blur", () => { //No sé cómo cambiar color
+    if (!(validateCheckbox())) {
+        checkbox.style.outlineColor = "red";
+    }
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if(validateName() && validateMail() && validateCheckbox()) {
+        //form.submit();  //Comentado para no ser redirigido si se hacee el submit
+        nombre.style.borderColor = "#95989A";  //Reset de campos y estilos
+        email.style.borderColor = "#95989A";
+        nombre.placeholder = "";
+        email.placeholder = "";
+        checkbox.checked = false;
+        alert("We'll keep you updated!"); //Para comprbar que funciona el "submit"
+    }
+    nombre.value = "";
+    email.value = "";
+}, false);
+
+function validateName() {  
+    if (nombre.value.length < 2 || nombre.value.length > 100) {
+        nombre.placeholder = "Name field must contain between 2 and 100 characters";
+        return false;
+    } else {
+        return true;
+    }   
+}
+
+function validateMail() {  //Debería haber hecho estas funciones con parámetros para comprobar también el input del modal pero no me da la vida
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(form.email.value)) {
+        return true;
+    } else {
+        email.placeholder = "example@mail.com"
+        return false;
+    }
+}
+
+function validateCheckbox() {
+    if (checkbox.checked) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Selector de moneda
+
+const currencySelector = document.getElementById("currency");
+const basicPrice = document.getElementById("basic-price");
+const proPrice = document.getElementById("pro-price");
+const premiumPrice = document.getElementById("premium-price");
+
+let oldValue = currencySelector.value;
+
+currencySelector.addEventListener("change", async() => {  //Seguramente el algoritmo más ridículo para resolver esto, cuando tenga una base sólida de la página lo intentaré optimizar
+
+    let newValue = currencySelector.value;
+    let symbol = "";
+
+    if (newValue === "eur") {
+        symbol = "€";
+    } else if (newValue === "usd") {
+        symbol = "$";
+    } else {
+        symbol = "£";
+    }
+
+    try { //Hay que hacer la llamada a la API en cada cambio de moneda para obtener el dato, en una función externa devuelve promesa siempre
+        let response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${oldValue}/${newValue}.json`);
+        if (response.ok) {
+            let data = await response.json();
+            let exchange = data[newValue];
+            let pro = Math.round(25 * exchange);
+            let premium = Math.round(60 * exchange);
+
+            basicPrice.innerHTML = `${symbol}0`;
+            proPrice.innerHTML = `${symbol}${pro}`;
+            premiumPrice.innerHTML = `${symbol}${premium}`;
+        } else {
+            console("Error en la petición");
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
+});
+
+
+//Modal
+
+const storage = window.sessionStorage;
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close-modal");
+const formDialog = document.getElementById("form-dialog");
+const emailDialog = document.getElementById("subscribe");
+
+window.addEventListener("load", () => {
+    if (!(modal.open) && storage.getItem("visit") == null) {
+        storage.setItem("visit", true);
+        setTimeout(()=>{
+            modal.showModal();
+         },5000);
+    }
+    
+});
+
+window.addEventListener("scroll", () => {
+    let percent = calcPercentage();
+    if (storage.getItem("visit") == null && percent >= 25 && (!(modal.open))) {
+
+        storage.setItem("visit", true);
+        modal.showModal();
+    }
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.close();
+});
+
+
+formDialog.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formDialog.subscribe.value)) {
+        //formDialog.submit();  //Comentado para no ser redirigido si se hacee el submit
+        modal.close();
+        alert("Thank you!");  //Para comprbar que funciona el "submit"
+    } else {
+        emailDialog.value = "";
+        emailDialog.placeholder = "example@mail.com";
+        emailDialog.style.borderColor = "red";
+    }
+});
